c74fac9f873b56237540049c92338b6aeb583000
손민석 <sonminseok@sonminseog-ui-MacBookAir.local>
2026-01-07 17:37:53 +0900

    섹션6. 이벤트로 처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,10 @@
 export default function Button({ children, text, color = 'black' }) {
+  const onClickButton = (e) => {
+    console.log(e); // 합성 이벤트 객체
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button style={{ color: color }} onClick={onClickButton}>
       {text} - {color.toUpperCase()}
       {children}
     </button>
