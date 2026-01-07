37dcf5410369a9c8ec6581b9099ec67dbd244fc6
손민석 <sonminseok@sonminseog-ui-MacBookAir.local>
2026-01-07 17:13:58 +0900

    섹션6. jsx로 UI 표현하기

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,7 +1,18 @@
+import './Main.css';
+// jsx 주의사항
+// 1. 중괄호 내부에는 표현식만 작성 가능하다. 조건문, 반복문 등 안됨.
+// 2. 숫자, 문자열, 배열 값만 렌더링 된다.
 export default function Main() {
-  return (
-    <main>
-      <h1>main</h1>
-    </main>
-  );
+  const user = {
+    name: 'Son',
+    isLogin: true,
+  };
+
+  if (user.isLogin) {
+    return <div className="logout">로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+
+  // return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 }
