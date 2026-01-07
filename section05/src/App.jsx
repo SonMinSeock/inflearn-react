2fa6121c2639a0747b59eb45c6662f8325661671
손민석 <sonminseok@sonminseog-ui-MacBookAir.local>
2026-01-07 18:48:04 +0900

    섹션6. State와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from 'react';
 import './App.css';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
+
+// 리액트 컴포넌트 리렌더링 조건 (불필요한 리렌더링 방지할려면 아래 조건 확인 해야한다.)
+// 1. state
+// 2. props
+// 3. 부모 컴포넌트 리렌더링 했을때
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === 'ON' ? 'OFF' : 'ON');
-          }}
-        >
-          {light === 'ON' ? '끄기' : '켜기'}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
