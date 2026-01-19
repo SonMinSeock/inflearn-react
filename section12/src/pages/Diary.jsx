3244190a26673bdc3aadb55d26810db07032527b
손민석 <sonminseok@sonminseog-ui-MacBookAir.local>
2026-01-19 11:22:12 +0900

    섹션 13. 페이지 라우팅 - 동적 경로

--- section12/src/pages/Diary.jsx
+++ section12/src/pages/Diary.jsx
@@ -1,3 +1,8 @@
+import { useParams, useLocation } from 'react-router-dom';
+
 export default function Diary() {
-  return <div>Diary</div>;
+  // 라우터에 정의한 params를 받고 싶을때, useParams 훅을 이용하여 params 객체를 반환해준다.
+  const params = useParams();
+
+  return <div>{params.id}번 일기입니다!</div>;
 }
